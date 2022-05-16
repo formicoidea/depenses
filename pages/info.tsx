cdfc5cc73d498fe5179ae016dc640de674592bf4
Mathieu Jolly <mathieu.jolly@protonmail.com>
2022-05-16 15:58:26 +0200

    liste des améliorations

--- pages/info.tsx
+++ pages/info.tsx
@@ -15,6 +15,9 @@ const InfoPage = () => {
                 <li>Exclure automatiquement les dépenses qui sont supérieur à plus de deux écarts type de la moyenne mensuelle</li>
                 <li>Ajouter une propriété de prise en compte de la dépense dans le notion</li>
                 <li>Filtrer en fonction de cette propriété marqueuse</li>
+                <br />
+                <li>Intégrer à tricount</li>
+                <li>Intégrer à la caisse d'épargne</li>
             </ul>
             <Info />
         </div>
